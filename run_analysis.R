9d18bf609455915b69806238e189c080d6b58cec
Steffen278 <steffen_menne@web.de>
2014-09-13 12:54:44 +0200

    Step 5: Tidy data set with mean of each combination

--- run_analysis.R
+++ run_analysis.R
@@ -157,7 +157,7 @@ merged_set <- arrange(merged_set, subject_id, activity_labels
 # Step 5: Creating a seperate tidy data set with the average of each variable for each activity and each subject #
 ##################################################################################################################
 
-tidyset <- ddply(merged_set, .(subject_id, activity_labels, measurement_type), summarise, mean_measurement=mean(measurement_value))
+tidy_set <- ddply(merged_set, .(subject_id, activity_labels, measurement_type), summarise, mean_measurement=mean(measurement_value))
 
 ## Final cleanup to declutter the work space
 
